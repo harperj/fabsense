ea2dd1168a50b98157fac30860ab3b89239192cb
timcam <timcam@berkeley.edu>
2013-12-01 17:31:57 -0800

    changed the codec to THEORA in the video recording file

--- Data Recording /testcv2.py
+++ Data Recording /testcv2.py
@@ -1,18 +1,18 @@
 import cv2 
 import time, sys
 
-print "Filename testVideo-timestamp-6.txt"
-f = open("testNoVideo-timestamp-6.txt", "w")
+print "Filename testVideo-timestamp-7.txt"
+f = open("testNoVideo-timestamp-7.txt", "w")
 
 
 camera = cv2.VideoCapture(0)
-fps = 25
+fps = 6
 capSize = (1028,720)
-fourcc = cv2.cv.CV_FOURCC('P','I','M','1') # note the lower case
+fourcc = cv2.cv.CV_FOURCC('T','H','E','O') # note the lower case
 _, img = camera.read()
 height, width, layers = img.shape
 
-video = cv2.VideoWriter('output.avi', fourcc, fps, (width, height))
+video = cv2.VideoWriter('output-7.ogv', fourcc, fps, (width, height))
 
 while True:
    _, img = camera.read()

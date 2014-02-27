d8f14dca38f3d68468eff93dac8772875fe8c776
timcam <timcam@berkeley.edu>
2014-02-27 04:25:44 -0800

    Continued to work on main matlab

--- src/matlab/scripts/main.m
+++ src/matlab/scripts/main.m
@@ -23,8 +23,9 @@ To Do:
 clear all; close all;
 
 %% import data and reformat
-filename = 'data/hammer_data1/testrun.csv';
-%filename = '../Data Recording //data/1/hammer-15';
+foldername = '../../../data/6-hammer/';
+trialnum = num2str(6);
+filename = [foldername  trialnum '-data.csv'];
 M = importdata(filename,',',1);
 
 global d;
@@ -45,7 +46,7 @@ for i = 1:numel(s)
         inc = inc+1;
     end 
 end
-clear s ax i j inc filename
+clear s i j inc filename
 
 %% format windows
 %{
@@ -57,47 +58,119 @@ and c. 4 is ending sample index.
 winSize = .25;
 start   = d.time(1);
 diff    = d.time(end) - start;
-bins    = ceil(diff/winSize);
+buckets = ceil(diff/winSize);
 
 %reformat time to 0 start
 d.time(:,2) = d.time - d.time(1);
-%build windows bins
-d.windows   = (0:winSize:diff)';
+%build bins
+d.bins = (0:winSize:diff)';
+
 %bin the data, returns bincount and indexes
-[d.windows(:,2),d.time(:,3)] ...
-     = histc(d.time(:,2),d.windows);
+[d.bins(:,2),d.time(:,3)] ...
+     = histc(d.time(:,2),d.bins);
 
 %locate the indicies
-for i = 1:bins
-    if (d.windows(i,2) ~= 0)
+for i = 1:buckets
+    if (d.bins(i,2) ~= 0)
         %find the start index in time
-        d.windows(i,3) = find(d.time(:,3) == i,1,'first');
+        d.bins(i,3) = find(d.time(:,3) == i,1,'first');
         %find the end index in time
-        d.windows(i,4) = find(d.time(:,3) == i,1,'last');
+        d.bins(i,4) = find(d.time(:,3) == i,1,'last');
     end
 end
 
-clear winSize start diff i
+%remove the 0 bins 
+remove = find(d.bins(:,2)==0);
+d.bins(remove,:) = [];
+buckets = buckets - length(remove);
+
+clear remove
+
+%% build window structures
+% places raw data in the d.window{i}.sensor format to be called later for
+% windowing and feature building
+
+%build windows, cell array contains split data
+d.windows = cell(buckets, 1);
+
+sen = {'acc','gyr','mag'};
+ax = {'x','y','z'};
+tempStruct = struct('acc',[],'gyr',[],'mag',[]);
+
+for i = 1:buckets
+    s = d.bins(i,3);
+    f = d.bins(i,4);
+
+    for j = 1:numel(sen)
+        %preallocating size of tmp
+        tmp = zeros(3,d.bins(i,2));
+        for k = 1:numel(ax)
+            %temp is all thres axes at one time
+           tmp(k,:) = (d.(sen{j}).(ax{k})(s:f))';
+        end
+        tempStruct.(sen{j})=tmp;
+    end   
+    d.windows{i} = tempStruct; 
+end
+
+clear winSize start diff i j k tmp tempStruct s f 
 
 %% build feauture vector and labels
 d.features = [];
 d.featurenames = {};
-d.labels = {};
+d.labels = repmat({'noise'},buckets,1);
 
 %% import annotations
-%d.ann.times = load('data/hammer_data1/annotations.csv');
-a = importdata('data/hammer_data1/annotations.csv',',');
-d.ann.times = a.data ./ 1000;
+filename = [foldername trialnum '-annotations.csv'];
+if(exist(filename,'file'))
+    a = importdata(filename,',');
+    d.ann.times  = a.data ./ 1000;
+    d.ann.labels = a.textdata;
 
-for i = 1:length(d.ann.times);
-    start  = d.ann.times(i,1);
-    finish = d.ann.times(i,2);
-    sindex = find(d.time >= start,1,'first');
-    findex = find(d.time >= finish,1,'first');
-    d.ann.examples{i,1} = d.all(sindex:findex,1:10);
+    for i = 1:length(d.ann.times);
+        start  = d.ann.times(i,1);
+        finish = d.ann.times(i,2);
+        sindex = find(d.time >= start,1,'first');
+        findex = find(d.time >= finish,1,'first');
+        d.ann.examples{i,1} = d.all(sindex:findex,1:10);
+    end
+else 
+    disp('no annotation file, setting flag and moving on');
+    no_annot_flag = true;
 end
 
-clear start finish i sindex findex
+clear start finish i sindex findex filename
+
+%% add annotations to labels 
+%make annotation times 0 sec start point
+times = d.ann.times-d.time(1);
+[len,~] = size(d.ann.times);
+
+for i = 1:len
+    s = find(d.bins(:,1) > times(i,1),1,'first');
+    f = find(d.bins(:,1) < times(i,2),1,'last');
+    for j = s:f
+        d.labels{j} = d.ann.labels{i};
+    end
+end
+
+clear a f s i j times len
+
+%% build rms features
+d.features = [d.features, zeros(buckets,9)];
+n = 1;
+
+for i = 1:buckets
+    for j = 1:numel(sen)
+        for k = 1:3
+           d.features(i,n) = rms(d.windows{i}.(sen{j})(k,:));
+           n = n+1;
+        end
+    end
+    n = 1;
+end
+
+clear n i j k 
 
 %% build the rms signal 
 %{
@@ -105,20 +178,20 @@ for each signal acc, gyr, and mag, we take the x,y, and z values at each
 point and find the magnitude, which is the root sum of the squares. In this
 case we use rms since the value of n is always 3.
 %}
-s = {'acc','gyr','mag'};
+sen = {'acc','gyr','mag'};
 ax = {'x','y','z'};
 tmp = zeros(1,3);
 
 %loop through each sensor, then each timestamp, then each axis
-for i = 1:numel(s)
+for i = 1:numel(sen)
     for j = 1:length(d.time)
         for k = 1:numel(ax)
             %temp is all thres axes at one time
-            tmp(k) = d.(s{i}).(ax{k})(j);
+            tmp(k) = d.(sen{i}).(ax{k})(j);
         end
         %here's where the magic happens
-        d.(s{i}).rms(j,1) = rms(tmp);
+        d.(sen{i}).rms(j,1) = rms(tmp);
     end   
 end
 
-clear i j k tmp
+clear i j k tmp ax
